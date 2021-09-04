00af25e5737cebe02a497454ff0a6ea4c03c1c5f
gboliknow <gboliknow@gmail.com>
2021-09-04 17:07:19 -0200

    feat : fixed listview in workspace

---
+++ lib/app/app.router.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import '../ui/view/login/login_view.dart';
+import '../ui/view/nav_bar/nav_bar_view.dart';
+import '../ui/view/preference/preference_view.dart';
+import '../ui/view/workspace/workspace_view.dart';
+
+class Routes {
+  static const String navBarView = '/';
+  static const String loginView = '/login-view';
+  static const String preferenceView = '/preference-view';
+  static const String workspaceView = '/workspace-view';
+  static const all = <String>{
+    navBarView,
+    loginView,
+    preferenceView,
+    workspaceView,
+  };
+}
+
+class StackedRouter extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.navBarView, page: NavBarView),
+    RouteDef(Routes.loginView, page: LoginView),
+    RouteDef(Routes.preferenceView, page: PreferenceView),
+    RouteDef(Routes.workspaceView, page: WorkspaceView),
+  ];
+  @override
+  Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, StackedRouteFactory>{
+    NavBarView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const NavBarView(),
+        settings: data,
+      );
+    },
+    LoginView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const LoginView(),
+        settings: data,
+      );
+    },
+    PreferenceView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const PreferenceView(),
+        settings: data,
+      );
+    },
+    WorkspaceView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const WorkspaceView(),
+        settings: data,
+      );
+    },
+  };
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stacked_themes/stacked_themes.dart';
+
+
+import 'app/app.locator.dart';
+import 'app/app.router.dart';
+import 'services/theme_setup.dart';
+
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ThemeManager.initialise();
+  setupLocator();
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ThemeBuilder(
+      themes: getThemes(),
+      builder: (context, regularTheme, darkTheme, themeMode) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorKey: StackedService.navigatorKey,
+        onGenerateRoute: StackedRouter().onGenerateRoute,
+        title: 'ZuriChat App',
+        theme: regularTheme,
+        darkTheme: darkTheme,
+        themeMode: themeMode,
+        initialRoute: Routes.navBarView,
+      ),
+    );
+  }
+}
